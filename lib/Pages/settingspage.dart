e0fd80f8598dd1ed26a489eb246df8c68ff52ef3
Himmet Arda <sekizarda4343@hotmail.com>
2023-11-27 21:45:26 +0300

    settings page eklendi

--- lib/Pages/settingspage.dart
+++ lib/Pages/settingspage.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 import 'package:habbitty/Pages/dailyquotespage.dart';
 import 'package:habbitty/Pages/help.dart';
@@ -18,6 +20,55 @@ class SettingsPage extends StatelessWidget {
         backgroundColor: lightOrange,
         title: const Text("Settings"),
       ),
+      body: Column(
+        children: [
+          Expanded(
+            child: SingleChildScrollView(
+              scrollDirection: Axis.vertical,
+              child: Column(
+                children: [
+                  SettingsElements(
+                      Icons.notifications_outlined,
+                      "Notifications",
+                      Icons.info_outline,
+                      "\u{1F6C8} Bildirimleri açıp kapatma",
+                      useSwitch: true),
+                  Divider(
+                    thickness: 3,
+                  ),
+                  SettingsElements(Icons.vibration, "Vibration",
+                      Icons.info_outline, "\u{1F6C8} Titreşim açıp kapatma",
+                      useSwitch: true),
+                  Divider(
+                    thickness: 3,
+                  ),
+                  SettingsElements(Icons.brush_outlined, "Customize",
+                      Icons.info_outline, "\u{1F6C8} Tema Seçenekleri",
+                      useSwitch: false),
+                  Divider(
+                    thickness: 3,
+                  ),
+                  SettingsElements(Icons.language, "Language",
+                      Icons.info_outline, "\u{1F6C8} Dil seçenekleri",
+                      useSwitch: false),
+                  Divider(
+                    thickness: 3,
+                  ),
+                  SettingsElements(
+                      Icons.sd_storage_outlined,
+                      "Data Reset & Restart",
+                      Icons.info_outline,
+                      "\u{1F6C8} Verileriniz sıfırlanır",
+                      useSwitch: false),
+                  Divider(
+                    thickness: 3,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
       drawer: Drawer(
         child: Container(
           color: Colors.white,
@@ -28,15 +79,19 @@ class SettingsPage extends StatelessWidget {
                 child: DrawerHeader(
                   padding: const EdgeInsets.all(0.0),
                   child: Container(
-                    decoration: BoxDecoration(
-                      color: lightOrange
-                    ),
+                    decoration: BoxDecoration(color: lightOrange),
                     child: const Center(
                       child: Column(
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
-                          Icon(Icons.account_circle_outlined, size: 100, color: Colors.white,),
-                          Text("<User Name>", style: TextStyle(fontSize: 24, color: Colors.white))
+                          Icon(
+                            Icons.account_circle_outlined,
+                            size: 100,
+                            color: Colors.white,
+                          ),
+                          Text("<User Name>",
+                              style:
+                                  TextStyle(fontSize: 24, color: Colors.white)),
                         ],
                       ),
                     ),
@@ -46,78 +101,252 @@ class SettingsPage extends StatelessWidget {
               // HOME
               ListTile(
                 contentPadding: const EdgeInsets.symmetric(horizontal: 30),
-                leading: Icon(Icons.home_rounded, color: navyBlue.withOpacity(0.5), size: 40,),
-                title: Text("Home", style: TextStyle(fontSize: 25, color: navyBlue.withOpacity(0.5))),
+                leading: Icon(
+                  Icons.home_rounded,
+                  color: navyBlue.withOpacity(0.5),
+                  size: 40,
+                ),
+                title: Text("Home",
+                    style: TextStyle(
+                        fontSize: 25, color: navyBlue.withOpacity(0.5))),
                 onTap: () {
-                  Navigator.of(context).push(MaterialPageRoute(builder: (context) => const HomePage()));
+                  Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => const HomePage()));
                 },
               ),
               // STATS
               ListTile(
                 contentPadding: const EdgeInsets.symmetric(horizontal: 30),
-                leading: Icon(Icons.equalizer_rounded, color: navyBlue.withOpacity(0.5), size: 40,),
-                title: Text("Stats", style: TextStyle(fontSize: 25, color: navyBlue.withOpacity(0.5))),
+                leading: Icon(
+                  Icons.equalizer_rounded,
+                  color: navyBlue.withOpacity(0.5),
+                  size: 40,
+                ),
+                title: Text("Stats",
+                    style: TextStyle(
+                        fontSize: 25, color: navyBlue.withOpacity(0.5))),
                 onTap: () {
-                  Navigator.of(context).push(MaterialPageRoute(builder: (context) => const StatsPage()));
+                  Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => const StatsPage()));
                 },
               ),
               // PROFILE
               ListTile(
                 contentPadding: const EdgeInsets.symmetric(horizontal: 30),
-                leading: Icon(Icons.account_circle_outlined, color: navyBlue.withOpacity(0.5), size: 40,),
-                title: Text("Profile", style: TextStyle(fontSize: 25, color: navyBlue.withOpacity(0.5))),
+                leading: Icon(
+                  Icons.account_circle_outlined,
+                  color: navyBlue.withOpacity(0.5),
+                  size: 40,
+                ),
+                title: Text("Profile",
+                    style: TextStyle(
+                        fontSize: 25, color: navyBlue.withOpacity(0.5))),
                 onTap: () {
-                  Navigator.of(context).push(MaterialPageRoute(builder: (context) => const ProfilePage()));
+                  Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => const ProfilePage()));
                 },
               ),
               // STORE
               ListTile(
                 contentPadding: const EdgeInsets.symmetric(horizontal: 30),
-                leading: Icon(Icons.shopping_bag_outlined, color: navyBlue.withOpacity(0.5), size: 40,),
-                title: Text("Store", style: TextStyle(fontSize: 25, color: navyBlue.withOpacity(0.5))),
+                leading: Icon(
+                  Icons.shopping_bag_outlined,
+                  color: navyBlue.withOpacity(0.5),
+                  size: 40,
+                ),
+                title: Text("Store",
+                    style: TextStyle(
+                        fontSize: 25, color: navyBlue.withOpacity(0.5))),
                 onTap: () {
-                  Navigator.of(context).push(MaterialPageRoute(builder: (context) => const StorePage()));
+                  Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => const StorePage()));
                 },
               ),
               // SETTINGS
               ListTile(
                 contentPadding: const EdgeInsets.symmetric(horizontal: 30),
-                leading: Icon(Icons.settings_outlined, color: lightOrange, size: 40,),
-                title: Text("Settings", style: TextStyle(fontSize: 25, color: lightOrange)),
+                leading: Icon(
+                  Icons.settings_outlined,
+                  color: lightOrange,
+                  size: 40,
+                ),
+                title: Text("Settings",
+                    style: TextStyle(fontSize: 25, color: lightOrange)),
                 onTap: () {
-                  Navigator.of(context).push(MaterialPageRoute(builder: (context) => const SettingsPage()));
+                  Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => const SettingsPage()));
                 },
               ),
               // QUOTE OF THE DAY
               ListTile(
                 contentPadding: const EdgeInsets.symmetric(horizontal: 30),
-                leading: Icon(Icons.format_quote, color: navyBlue.withOpacity(0.5), size: 40,),
-                title: Text("Daily Quotes", style: TextStyle(fontSize: 25, color: navyBlue.withOpacity(0.5))),
+                leading: Icon(
+                  Icons.format_quote,
+                  color: navyBlue.withOpacity(0.5),
+                  size: 40,
+                ),
+                title: Text("Daily Quotes",
+                    style: TextStyle(
+                        fontSize: 25, color: navyBlue.withOpacity(0.5))),
                 onTap: () {
-                  Navigator.of(context).push(MaterialPageRoute(builder: (context) => const DailyQuotePage()));
+                  Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => const DailyQuotePage()));
                 },
               ),
               // HELP
               ListTile(
                 contentPadding: const EdgeInsets.symmetric(horizontal: 30),
-                leading: Icon(Icons.question_mark_rounded, color: navyBlue.withOpacity(0.5), size: 40,),
-                title: Text("Help", style: TextStyle(fontSize: 25, color: navyBlue.withOpacity(0.5))),
+                leading: Icon(
+                  Icons.question_mark_rounded,
+                  color: navyBlue.withOpacity(0.5),
+                  size: 40,
+                ),
+                title: Text("Help",
+                    style: TextStyle(
+                        fontSize: 25, color: navyBlue.withOpacity(0.5))),
                 onTap: () {
-                  Navigator.of(context).push(MaterialPageRoute(builder: (context) => const HelpPage()));
+                  Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => const HelpPage()));
                 },
               ),
               // PRIVACY POLICY
               ListTile(
                 contentPadding: const EdgeInsets.symmetric(horizontal: 30),
-                leading: Icon(Icons.lock_outline, color: navyBlue.withOpacity(0.5), size: 40,),
-                title: Text("Privacy Policy", style: TextStyle(fontSize: 25, color: navyBlue.withOpacity(0.5))),
+                leading: Icon(
+                  Icons.lock_outline,
+                  color: navyBlue.withOpacity(0.5),
+                  size: 40,
+                ),
+                title: Text("Privacy Policy",
+                    style: TextStyle(
+                        fontSize: 25, color: navyBlue.withOpacity(0.5))),
                 onTap: () {
-                  Navigator.of(context).push(MaterialPageRoute(builder: (context) => const PrivacyPolicyPage()));
+                  Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => const PrivacyPolicyPage()));
                 },
               ),
             ],
           ),
         ),
+      ),
+    );
+  }
+}
+
+class SwitchExample extends StatefulWidget {
+  const SwitchExample({super.key});
+
+  @override
+  State<SwitchExample> createState() => _SwitchExampleState();
+}
+
+Container SettingsElements(ikon, name, info, icerik, {bool useSwitch = true}) {
+  Color navyBlue = const Color(0xFF122663);
+  return Container(
+    child: Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+        Row(
+          children: [
+            Icon(
+              ikon,
+              size: 30,
+              color: navyBlue,
+            ),
+            SizedBox(
+              width: 10,
+            ),
+            Text(
+              name,
+              style: TextStyle(fontSize: 25, color: navyBlue),
+            ),
+          ],
+        ),
+        Row(
+          children: [
+            useSwitch
+                ? SwitchExample()
+                : Container(), // Burada SwitchExample'ı kullanıp kullanmamayı kontrol ediyoruz
+            TooltipSample(
+              icerik: icerik,
+            )
+          ],
+        )
+      ]),
+    ),
+  );
+}
+
+class _SwitchExampleState extends State<SwitchExample> {
+  bool light = true;
+  Color navyBlue = const Color(0xFF122663);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        boxShadow: [
+          BoxShadow(
+            color: navyBlue.withOpacity(0.2),
+            spreadRadius: 0.1,
+            blurRadius: 1,
+          ),
+        ],
+        borderRadius: const BorderRadius.all(Radius.circular(20)),
+      ),
+      child: Switch(
+        // This bool value toggles the switch.
+        value: light,
+        activeColor: Color.fromARGB(255, 255, 87, 23),
+        activeTrackColor: Colors.white,
+        inactiveTrackColor: Colors.white,
+        onChanged: (bool value) {
+          // This is called when the user toggles the switch.
+          setState(() {
+            light = value;
+          });
+        },
+      ),
+    );
+  }
+}
+
+class TooltipSample extends StatelessWidget {
+  final String icerik;
+
+  const TooltipSample({Key? key, required this.icerik, required})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Color navyBlue = const Color(0xFF122663);
+    return Tooltip(
+      message: icerik,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        boxShadow: [
+          BoxShadow(
+            color: navyBlue.withOpacity(0.5),
+            spreadRadius: 3,
+            blurRadius: 5,
+          ),
+        ],
+        gradient: const LinearGradient(colors: <Color>[
+          Color.fromARGB(255, 255, 255, 255),
+          Color.fromARGB(255, 255, 255, 255)
+        ]),
+      ),
+      height: 50,
+      padding: const EdgeInsets.all(8.0),
+      preferBelow: false,
+      textStyle: const TextStyle(
+        fontSize: 20,
+        color: Color(0xFF122663),
+      ),
+      showDuration: const Duration(milliseconds: 70),
+      waitDuration: const Duration(seconds: 1),
+      child: const Icon(
+        Icons.info_outline,
+        size: 30,
       ),
     );
   }
