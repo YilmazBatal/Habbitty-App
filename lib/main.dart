22b001487bc795575bf1d3f0f92cd49e7c6fb121
YilmazBatal <b.ogulbey@gmail.com>
2023-12-07 15:56:16 +0300

    UI Changes

--- lib/main.dart
+++ lib/main.dart
@@ -12,22 +12,22 @@ Color lightGreen = const Color.fromARGB(255, 29, 192, 62); // Green 1
 Color darkGreen = const Color.fromARGB(255, 12, 126, 23); // Green 2
 Color navyBlue = const Color(0xFF122663); // Navy 1
 
-int credits = 100;
-int streak = 4;
-int completed = 3;
-int toBeCompleted = 20;
-String welcomeMessage = "Good Morning!";
 
 List todaysHabitList = [
   // [name,time,icon,isDone]
-  ["10x Push Ups", "10:00 AM - 10:15 AM", Icons.fitness_center_rounded, true],
-  ["Study Math", "12:00 PM - 2:15 PM", Icons.school_rounded, true],
-  ["Practice Programming", "3:00 PM - 6:00 PM", Icons.code_rounded, false],
-  ["Night Routine", "8:00 PM - 8:15 PM", Icons.bed_rounded, false],
-  ["Night Routine", "8:00 PM - 8:15 PM", Icons.bed_rounded, false],
-  ["Night Routine", "8:00 PM - 8:15 PM", Icons.bed_rounded, false],
-  ["Night Routine", "8:00 PM - 8:15 PM", Icons.bed_rounded, false],
+  [0,"10x Push Ups", "10:00 AM - 10:15 AM", Icons.fitness_center_rounded, true],
+  [1,"Study Math", "12:00 PM - 2:15 PM", Icons.school_rounded, true],
+  [2,"Practice Programming sssssss", "3:00 PM - 6:00 PM", Icons.code_rounded, false],
+  [3,"Night Routine", "8:00 PM - 8:15 PM", Icons.bed_rounded, false],
+  [4,"Bike Tour", "8:00 PM - 8:15 PM", Icons.pedal_bike_rounded, false],
+  [5,"Brushing Teeths", "8:00 PM - 8:15 PM", Icons.bed_rounded, false],
 ];
+
+int credits = 24;
+int streak = 4;
+int completed = 2;
+int toBeCompleted = todaysHabitList.length;
+String welcomeMessage = "Good Morning!";
 
 void main() {
   runApp(const MyApp());
